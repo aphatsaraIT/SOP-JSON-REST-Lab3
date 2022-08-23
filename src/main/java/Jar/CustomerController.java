315dd1fd22937a7342950eb92dc3d87cab1fa0a5
Aphatsara Moratsathian <63070188@kmitl.ac.th>
2022-08-23 22:39:40 +0700

    lab3

--- src/main/java/Jar/CustomerController.java
+++ src/main/java/Jar/CustomerController.java
@@ -1,25 +1,78 @@
 package Jar;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class CustomerController {
     private List<Customer> customers;
+
+    public CustomerController() {
+        customers = new ArrayList<Customer>();
+        Customer customer1 = new Customer("1010", "John", "Male", 25);
+        Customer customer2 = new Customer("1018", "Peter", "Male", 24);
+        Customer customer3 = new Customer("1019", "Sara", "Female", 23);
+        Customer customer4 = new Customer("1110", "Rose", "Female", 23);
+        Customer customer5 = new Customer("1001", "Emma", "Female", 30);
+        customers.add(customer1);
+        customers.add(customer2);
+        customers.add(customer3);
+        customers.add(customer4);
+        customers.add(customer5);
+
+    }
+
+    @RequestMapping(value = "/customers", method = RequestMethod.GET)
     public List<Customer> getCustomers(){
-
+        return customers;
     }
-    public Customer getCustomerByID(String ID){
 
+    @RequestMapping(value = "/customerbyid/{id}", method = RequestMethod.GET)
+    public Customer getCustomerByID(@PathVariable("id") String ID){
+        for(int i = 0; i < this.customers.size() ; i++){
+            if(this.customers.get(i).getID().equals(ID)){
+                return this.customers.get(i);
+            }
+        }
+        return null;
     }
-    public Customer getCustomerByName(String n){
 
-    }
-    public boolean delCustomerByID(String ID){
+    @RequestMapping(value = "/customerbyname/{n}", method = RequestMethod.GET)
+    public Customer getCustomerByName(@PathVariable("n") String n){
+        for(int i = 0; i < this.customers.size() ; i++){
+            if(this.customers.get(i).getName().equals(n)){
+                return this.customers.get(i);
+            }
+        }
 
+        return null;
     }
-    public boolean delCustomerByName(String n){
 
+    @RequestMapping(value = "/customerDelByid/{id}", method = RequestMethod.DELETE)
+    public boolean delCustomerByID(@PathVariable("id") String ID){
+        for(int i = 0; i < this.customers.size() ; i++){
+            if(this.customers.get(i).getID().equals(ID)){
+                this.customers.remove(this.customers.get(i));
+                return true;
+            }
+        }
+        return false;
     }
-    public addCustomer(String ID, String n, String s, int a){
 
+    @RequestMapping(value = "/customerDelByname/{n}", method = RequestMethod.DELETE)
+    public boolean delCustomerByName(@PathVariable("n") String n){
+        for(int i = 0; i < this.customers.size() ; i++){
+            if(this.customers.get(i).getName().equals(n)){
+                this.customers.remove(this.customers.get(i));
+                return true;
+            }
+        }
+        return false;
     }
+
+
 }
